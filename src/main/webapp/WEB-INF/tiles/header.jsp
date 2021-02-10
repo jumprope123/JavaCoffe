fc5cab521d62771ad293da7a603101f2097e48dd
jumprope123 <70045183+jumprope123@users.noreply.github.com>
2021-02-10 17:49:15 +0900

    첫번째 뼈대

---
+++ src/main/webapp/WEB-INF/tiles/header.jsp
@@ -0,0 +1,33 @@
+<%@ page pageEncoding="UTF-8" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<header class="row">
+    <div class="col-6">
+        <h1><a href="/index" class="colblack">Lorem Ipsum</a></h1>
+    </div>
+    <div class="col-6 text-right">
+            <%-- 세션변수 UID의 값이 존재하지 않는다면 (즉, 로그인을 하지 않았다면) --%>
+            <c:if test="${empty UID}">
+        <h1>
+            <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#loginmodal">로그인</button>
+            <button type="button" class="btn btn-primary" id="join2btn">회원가입</button>
+        </h1>
+            </c:if>
+                <%-- 세션변수 UID의 값이 존재한다면 (즉, 로그인 했다면) --%>
+            <c:if test="${not empty UID}">
+        <h1>
+            <button type="button" class="btn btn-dark" id="logoutbtn">로그아웃</button>
+        </h1>
+            </c:if>
+    </div>
+</header>
+
+<nav class="nav navbar-expand navbar-dark bg-dark">
+    <ul class="nav navbar-nav nav-fill w-100">
+        <li class="nav-item"><a class="nav-link" href="#">소개</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">추천</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">액세서리</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">고객지원</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">뉴스</a></li>
+        <li class="nav-item"><a class="nav-link" href="#"">매장찾기</a></li>
+    </ul>
+</nav>
---
+++ src/main/webapp/resources/js/board.js
@@ -0,0 +1,90 @@
+// list
+$('#newbd').on('click',function () {
+    location.href = "/board/write";
+})
+
+$('#bdfindbtn').on('click',function () {
+    let param = "?findtype=" + $('#findtype').val();
+    param += "&findkey=" + $('#findkey').val();
+    location.href = "/board/find" + param + '&cp=1';
+})
+
+// view
+$('#listbdbtn').on('click', function () {
+    // history.back(); // 비추
+    // location.href = "/board/list?cp=1";  // 첫 페이지로
+    location.href = "/board/list?cp=" + $('#cp').val();
+})
+
+$('#thumbbtn').on('click', function () {
+    location.href = "board/ "
+})
+
+$('#upbdbtn').on('click', function () {
+    if (confirm('정말로 이 글을 수정하시겠습니까?')) {
+        let param = "?bno=" + $('#bno').val();
+        param += "&cp=" + $('#cp').val()
+        param += "&userid=" + $('#userid').val();
+        location.href = '/board/update' + param;
+    }
+})
+
+$('#rmbdbtn').on('click', function () {
+    if (confirm('정말로 이 글을 삭제하시겠습니까?')) {
+        let param = '?bno=' + $('#bno').val();
+        param += "&cp=" + $('#cp').val()
+        param += "&userid=" + $('#userid').val();
+        location.href = '/board/delete' + param;
+    }
+})
+
+$('#bdcmtbtn').on('click',function () {
+    if ($('#reply').val() == '') alert('댓글을 작성하세요');
+    else {
+        $('#uid').val('테스트계정');
+        $('#replyfrm').attr('method','post');
+        $('#replyfrm').attr('action','/board/replyok');
+        $('#replyfrm').submit();
+    }
+})
+
+function addReply(cno) {
+    $('#replyModal').modal('show');
+    $('#cno').val(cno); // 대댓글 작성시 부모댓글의 번호를 cno에 저장
+} //대댓글 대화상자 띄우기
+
+$('#rpbtn').on('click',function () {
+    if ($('#rereply').val() == '') alert('내용을 작성하세요');
+    else {
+        $('#rpfrm').attr('method','post');
+        $('#rpfrm').attr('action','/board/replyok');
+        $('#rpfrm').submit();
+    }
+}) // 대댓글 작성하기
+
+
+
+// write
+$('#newbdbtn').on('click',function () {
+    if ($('#title').val() == '') alert('제목을 작성하세요!');
+    else if ($('#contents').val() == '') alert('본문을 작성하세요!');
+    else {
+        $('#newbdfrm').attr('method','post');
+        $('#newbdfrm').attr('action','/board/write');
+        $('#newbdfrm').submit();
+    }
+})
+
+$('#upbdokbtn').on('click',function () {
+    if ($('#title').val() == '') alert('제목을 작성하세요!');
+    else if ($('#contents').val() == '') alert('본문을 작성하세요!');
+    else { (confirm('정말로 수정 하시겠습니까?'))
+        $('#upbdfrm').attr('method','post');
+        $('#upbdfrm').attr('action','/board/update');
+        $('#upbdfrm').submit();
+    }
+});
+
+$('#ccupbdbtn').on('click',function () {
+    history.back();
+});
