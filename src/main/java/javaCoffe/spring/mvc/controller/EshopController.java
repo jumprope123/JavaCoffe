a871bf73ae49f22fed258315f9493e3241ff3d9d
jumprope123 <70045183+jumprope123@users.noreply.github.com>
2021-03-01 20:17:01 +0900

    Eshop 페이지 수정 및 상단 네비바 동작구현, 이후 중단 네비바 및 xx개씩 보기 구현예정

--- src/main/java/javaCoffe/spring/mvc/controller/EshopController.java
+++ src/main/java/javaCoffe/spring/mvc/controller/EshopController.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.List;
 
 @Controller
 public class EshopController {
@@ -17,10 +16,10 @@ public class EshopController {
     @GetMapping("/eshop/list")
     public ModelAndView list(ModelAndView mv, String cp, String bigGenre){
         mv.setViewName("eshop/list.tiles");
-        if (bigGenre.equals("coffe")){ // 대분류값이 coffe일때
-            mv.addObject("bigGenres",esrv.readCoffeInfo(bigGenre,cp)); // cp값에따라 20개의 데이터를 넘김
-            mv.addObject("bigGenreCnt",esrv.countBigGenre(bigGenre)); // 해당 대분류값의 총 갯수를 파악함
-        }
+
+        mv.addObject("bigGenres",esrv.readCoffeInfo(bigGenre,cp)); // cp값에따라 20개의 데이터를 넘김
+        mv.addObject("bigGenreCnt",esrv.countBigGenre(bigGenre)); // 해당 대분류값의 총 갯수를 파악함
+
         return mv;
     }
 
