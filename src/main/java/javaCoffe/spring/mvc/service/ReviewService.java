c93684e17183bad1b3a9f3fdfedf162f8e5b2ce2
jumprope123 <70045183+jumprope123@users.noreply.github.com>
2021-03-01 17:08:54 +0900

    메인페이지 오류발생 파일추가

---
+++ src/main/java/javaCoffe/spring/mvc/service/ReviewService.java
@@ -0,0 +1,36 @@
+package javaCoffe.spring.mvc.service;
+
+import javaCoffe.spring.mvc.vo.ReviewVO;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+public interface ReviewService {
+    Boolean newReview(ReviewVO rvo, MultipartFile[] img);
+
+    List<ReviewVO> readReview(String cp);
+
+    List<ReviewVO> readReview(String cp, String findtype, String findkey);
+
+    int countReview();
+
+    int countReview(String findtype, String findkey);
+
+    ReviewVO readOneReview(String rno);
+
+    Boolean viewCountReview(String rno);
+
+    Boolean delReview(ReviewVO nrvo);
+
+    Boolean updateReview(ReviewVO rvo, MultipartFile[] img, String[] fileck);
+
+    String readPreReview(String rno);
+
+    String readFirstRno();
+
+    String readNextReview(String rno);
+
+    String readLastRno();
+
+    String updateThumb(String rno, String checkThumb);
+}
