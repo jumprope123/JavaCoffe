d4fa04d90a2be89e087754d797e3ab48c63dbe15
jumprope123 <70045183+jumprope123@users.noreply.github.com>
2021-03-08 14:40:38 +0900

    주문진행페이지 초안

--- src/main/java/javaCoffe/spring/mvc/dao/AdminDAO.java
+++ src/main/java/javaCoffe/spring/mvc/dao/AdminDAO.java
@@ -1,7 +1,12 @@
 package javaCoffe.spring.mvc.dao;
 
+import javaCoffe.spring.mvc.vo.BuyPageVO;
 import javaCoffe.spring.mvc.vo.EshopVO;
+
+import java.util.List;
 
 public interface AdminDAO {
     String insertNewProduct(EshopVO evo);
+
+    List<BuyPageVO> readAllData();
 }
--- src/main/java/javaCoffe/spring/mvc/service/AdminService.java
+++ src/main/java/javaCoffe/spring/mvc/service/AdminService.java
@@ -1,8 +1,13 @@
 package javaCoffe.spring.mvc.service;
 
+import javaCoffe.spring.mvc.vo.BuyPageVO;
 import javaCoffe.spring.mvc.vo.EshopVO;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+
 public interface AdminService {
     boolean newProduct(EshopVO evo, MultipartFile[] img);
+
+    List<BuyPageVO> readBuyData();
 }
