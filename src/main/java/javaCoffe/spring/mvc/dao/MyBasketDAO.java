bb50afc5b38b11d6889b90cda8e925112d6d3d81
tjdxo <tjdxo11933@naver.com>
2021-03-09 07:30:43 +0900

    mybasket 초기 구현

--- src/main/java/javaCoffe/spring/mvc/dao/MyBasketDAO.java
+++ src/main/java/javaCoffe/spring/mvc/dao/MyBasketDAO.java
@@ -1,4 +1,14 @@
 package javaCoffe.spring.mvc.dao;
 
+import javaCoffe.spring.mvc.vo.MyBasketVO;
+
+import java.util.List;
+import java.util.Map;
+
 public interface MyBasketDAO {
+    List<MyBasketVO> selectmb(String userid);
+
+    int insertmb(Map<String, Object> param);
+
+//    MyBasketVO selectOnemb(Map<String, Object> param);
 }
--- src/main/java/javaCoffe/spring/mvc/dao/MyBasketDAOImpl.java
+++ src/main/java/javaCoffe/spring/mvc/dao/MyBasketDAOImpl.java
@@ -1,8 +1,12 @@
 package javaCoffe.spring.mvc.dao;
 
+import javaCoffe.spring.mvc.vo.MyBasketVO;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Map;
 
 @Repository("mbdao")
 public class MyBasketDAOImpl implements MyBasketDAO{
@@ -10,4 +14,18 @@ public class MyBasketDAOImpl implements MyBasketDAO{
     private SqlSession sqlSession;
 
 
+    @Override
+    public List<MyBasketVO> selectmb(String userid) {
+        return sqlSession.selectList("mybasket.selectListmb", userid);
+    }
+
+    @Override
+    public int insertmb(Map<String, Object> param) {
+        return sqlSession.insert("mybasket.insertmb", param);
+    }
+
+//    @Override
+//    public MyBasketVO selectOnemb(Map<String, Object> param) {
+//        return sqlSession.selectOne("mybasket.selectOnemb", param);
+//    }
 }
