690db07b4cf8f099f7a7adab0a99e2a96e26cf56
Shin JiHwan <70045183+jumprope123@users.noreply.github.com>
2021-03-10 18:56:20 +0900

    Update CoAndMaController.java
    오류수정

--- src/main/java/javaCoffe/spring/mvc/controller/CoAndMaController.java
+++ src/main/java/javaCoffe/spring/mvc/controller/CoAndMaController.java
@@ -41,25 +41,6 @@ public class CoAndMaController {
     }
 
 
-    @GetMapping("coffemachine/view2")
-    public ModelAndView description1(ModelAndView mv, String kof){
-
-
-        mv.setViewName("coffemachine/view2");
-
-
-        return mv;
-    }
-    @GetMapping("coffemachine/view1")
-    public ModelAndView description2(ModelAndView mv, String kof){
-
-
-        mv.setViewName("coffemachine/view1");
-
-
-        return mv;
-    }
-
 
     @GetMapping("/CoffeeAndMachine/researchPrint")
     public ModelAndView researchPrint(ModelAndView mv, String kof){
